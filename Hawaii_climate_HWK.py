4db7bdad326de3eaef060efaf19fcf3e15e17f97
Leticia Salazar <52460414+letisalbal@users.noreply.github.com>
2019-10-05 11:59:43 -0400

    Update Hawaii_climate_HWK.py

--- Hawaii_climate_HWK.py
+++ Hawaii_climate_HWK.py
@@ -1,8 +1,4 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import numpy as np
-
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
